754aac2e438e8dd1f03f3c86d9264b0cc72c5cda
pradojeric <jericprado0719@gmail.com>
2021-12-14 12:28:26 +0800

    new

--- app/Http/Controllers/FacultyController.php
+++ app/Http/Controllers/FacultyController.php
@@ -109,7 +109,9 @@ class FacultyController extends Controller
             $query->where('faculty_id', $faculty->id);
         });
 
-        $schedules = resolve(ScheduleService::class)->getTimeSchedules($r, true);
+        // $schedules = resolve(ScheduleService::class)->getTimeSchedules($r, true);
+
+        $timeRange = CarbonInterval::minutes(30)->toPeriod('7:00', '20:00');
         $days = [
             'M' => 'monday',
             'T' => 'tuesday',
@@ -119,6 +121,31 @@ class FacultyController extends Controller
             'SAT' => 'saturday',
             'SUN' => 'sunday'
         ];
+        $data = [];
+        foreach($timeRange as $time)
+        {
+            $t = $time->format('h:i A');
+
+
+            foreach($days as $day)
+            {
+                $exists = (clone $r)->where(function($query) use ($time){
+                            $query->where('start', '<=', $time)
+                                ->where('end', '>', $time);
+                        })
+                        ->where($day, 1)
+                        ->first();
+
+                $data[$day] = $exists;
+            }
+
+
+            $roomsAvailable["$t"] = $data;
+        }
+
+        $schedules = $roomsAvailable;
+
+
 
         return view('pages.faculties.show', compact('faculty', 'schedules', 'days'));
     }
