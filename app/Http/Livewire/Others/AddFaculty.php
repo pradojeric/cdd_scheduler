350f55f2f302716ed87bb830cb5373a9c452c888
pradojeric <jericprado0719@gmail.com>
2022-06-08 11:36:11 +0800

    test api

--- app/Http/Livewire/Others/AddFaculty.php
+++ app/Http/Livewire/Others/AddFaculty.php
@@ -106,7 +106,7 @@ class AddFaculty extends Component
             ->post($url.'/api/rooms/update/teacher', $step_room);
 
 
-        dd($response->throw());
+        dd($response);
 
 
         $this->close();
